8853ea0777cd32688171f41a2fb950989e6851f5
daesungYOON <anipen@Anipenui-MacBookPro.local>
2022-07-18 17:55:44 +0900

    계산기 기능 구현 완료 designable오류 수정 중

--- Calculator_FC/RoundButton.swift
+++ Calculator_FC/RoundButton.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
+// 변경된 값을 스토리보드에서 바로 확인 가능 속성
 @IBDesignable
 class RoundButton: UIButton {
 	
-	// 감시 프로퍼티를 사용함으로써 isRound에 값변화가 생기면 didSet실행
+  // 커스텀뷰 속성을 SB상 우측창에서 바로 적용 가능
 	@IBInspectable
+  // 감시 프로퍼티를 사용함으로써 isRound에 값변화가 생기면 didSet실행
 	var isRound: Bool = false {
 		didSet {
 			if isRound {
--- Calculator_FC/ViewController.swift
+++ Calculator_FC/ViewController.swift
@@ -21,10 +21,9 @@ class ViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
 	
-	// 라벨상에서 노출
+	// 라벨에 노출될 값 저장
 	var displayNumber = ""
 	
 	// 이전 계산값을 저장
@@ -38,38 +37,95 @@ class ViewController: UIViewController {
 	
 	// 현재 입력된 연산자의 값을 저장
 	var currentOperation: Operation = .unknown
-	
-	@IBAction func tapnumberButton(_ sender: UIButton) {
-		guard let numberValue = sender.title(for: .normal) else { return }
-		print("\(numberValue)")
-		if self.displayNumber.count < 9 {
-			self.displayNumber += numberValue
-			self.numberOutputLabel.text = self.displayNumber
-		}
-	}
-	
+  
+  @IBAction func tapNumberButton(_ sender: UIButton) {
+    // 강의상에서 알려준 sender.title사용시에 nil만 return하게 됨
+//  guard let numberValue = sender.title(for: .normal) else { return }
+    guard let numberValue = sender.titleLabel?.text else { return }
+    print("senderTitle : \(numberValue)")
+    if self.displayNumber.count < 9 {
+      self.displayNumber += numberValue
+      self.numberOutputLabel.text = self.displayNumber
+    }
+  }
+
 	@IBAction func tapClearButton(_ sender: UIButton) {
+    self.displayNumber = ""
+    self.firstOperand = ""
+    self.secondOperand = ""
+    self.result = ""
+    self.currentOperation = .unknown
+    self.numberOutputLabel.text = "0"
 	}
 	
 	
 	@IBAction func tapDotButton(_ sender: UIButton) {
+    // 프로퍼티가 8자리 미만이면서 소숫점을 포함하지 않을경우
+    if self.displayNumber.count < 8, !self.displayNumber.contains(".") {
+      self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
+      self.numberOutputLabel.text = self.displayNumber
+    }
 	}
 	
-	
 	@IBAction func tapDivideButton(_ sender: UIButton) {
+    self.operation(.Divide)
 	}
 	
 	@IBAction func tapMultiplyButton(_ sender: UIButton) {
+    self.operation(.Multiply)
 	}
 	
 	@IBAction func tapSubstractButton(_ sender: UIButton) {
+    self.operation(.Subtract)
 	}
 	
 	@IBAction func tapAddButton(_ sender: UIButton) {
+    self.operation(.Add)
 	}
 	
 	@IBAction func tapEqualButton(_ sender: UIButton) {
+    self.operation(self.currentOperation)
 	}
+  
+  // 계산 담당함수
+  func operation(_ operation: Operation) {
+    if self.currentOperation != .unknown {
+      if !self.displayNumber.isEmpty {
+        self.secondOperand = self.displayNumber
+        self.displayNumber = ""
+        
+        guard let firstOperand = Double(self.firstOperand) else { return }
+        guard let secondOperand = Double(self.secondOperand) else { return }
+        
+        switch self.currentOperation {
+        case .Add:
+          self.result = "\(firstOperand + secondOperand)"
+        case .Subtract:
+          self.result = "\(firstOperand - secondOperand)"
+        case .Divide:
+          self.result = "\(firstOperand / secondOperand)"
+        case .Multiply:
+          self.result = "\(firstOperand * secondOperand)"
+        default:
+          break
+        }
+        
+        // 소숫점 뒤에 0값이 들어올경우 소숫점 및 숫자 없애기
+        if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
+          self.result = "\(Int(result))"
+        }
+        
+        self.firstOperand = self.result
+        self.numberOutputLabel.text = self.result
+      }
+      
+      self.currentOperation = operation
+    } else {
+      self.firstOperand = self.displayNumber
+      self.currentOperation = operation
+      self.displayNumber = ""
+    }
+  }
 	
 }
 
